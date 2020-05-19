fcba67d7c8069fba09602bdb1d36fabf7b8cbb89
Asim Aslam <asim@aslam.me>
2020-05-19 17:52:58 +0100

    fix replace

--- projects/e.js
+++ projects/e.js
@@ -166,7 +166,7 @@ function loadRecent() {
 }
 
 function loadResults() {
-    var url = window.location.href.replace(/search.html/, "/api/search");
+    var url = window.location.href.replace(/search.html/, "api/search");
 
     $.ajax({url: url, 'type': "GET", dataType: 'json', success: function(data) {
         if (data.repos == undefined) {
